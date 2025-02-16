df97dfdb144b71b59f8a74d967b6fb5949a78c06
JEVITHESH <151340536+JEVITHESH@users.noreply.github.com>
2025-02-16 22:30:54 +0530

    Create Class_and_Object.dart

---
+++ Class_and_Object.dart
@@ -0,0 +1,11 @@
+class kailash{
+  int tea = 15;
+  int coffee = 20;
+  String hi ="Hi";
+}
+void main()
+{
+ kailash customer = kailash();
+   customer.hi ="Hi ,Welcome";
+   print(customer.hi);
+}
